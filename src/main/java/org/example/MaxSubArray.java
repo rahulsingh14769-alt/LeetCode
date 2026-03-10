b0f972cc9c912f2bd4bec121e46c33718fa80ec5
rahulsingh <rahulsingh14769@icloud.com>
2026-03-10 21:28:54 +0530

    Maximum Sub-Array - Leetcode

---
+++ src/main/java/org/example/MaxSubArray.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class MaxSubArray {
+    public int maxSubArray(int[] nums) {
+        int max_so_far = nums[0];
+        int curr_so_far = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            curr_so_far = Math.max(nums[i], curr_so_far + nums[i]);
+            max_so_far = Math.max(max_so_far, curr_so_far);
+        }
+        return max_so_far;
+    }
+}
