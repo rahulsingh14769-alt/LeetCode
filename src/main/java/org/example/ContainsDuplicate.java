e58464f4da961e4549eaca6f539793ad9692b779
rahulsingh14769-alt <rahulsingh14769@icloud.com>
2026-04-08 14:52:23 +0530

    Leetcode 217: Contains Duplicate

--- src/main/java/org/example/ContainsDuplicate.java
+++ src/main/java/org/example/ContainsDuplicate.java
@@ -1,4 +1,11 @@
+import java.util.HashSet;
+
 class ContainsDuplicate {
+    public static void main(String[] args) {
+        ContainsDuplicate solution = new ContainsDuplicate();
+        int[] nums = {1, 2, 3, 4, 2};
+        System.out.println(solution.containsDuplicate(nums)); // Output: true
+    }
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> seen = new HashSet<>();
         for(int num : nums) {
